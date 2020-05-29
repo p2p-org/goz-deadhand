93a49ebddb3f58418d8e4bc8391c9bff18d1e6ea
Vasiliy Shapovalov <vsh@MacBook-Pro-Vasiliy.local>
2020-05-29 20:06:50 +0300

    postgres

---
+++ msgs_to_postgeres.py
@@ -0,0 +1,32 @@
+import ijson
+import pprint
+import argparse
+import json
+import psycopg2
+
+conn = psycopg2.connect(dbname='postgres', user='postgres', 
+                        password='gozogozogoz', host='localhost')
+conn.autocommit = True
+cursor = conn.cursor()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("json_path", nargs='?', default="events.json")
+args = parser.parse_args()
+
+
+
+        
+different_structures = []
+first_occurence = {}
+
+f = open(args.json_path)
+msgs = ijson.items(f, '', multiple_values = True)
+i = 0
+#try: 
+for msg in msgs:
+    my_json = json.dumps(msg)
+    sql = "INSERT INTO json_test (data) VALUES (%s)"
+    cursor.execute(sql, (my_json,))
+#except:
+#    pass
+print(i)
