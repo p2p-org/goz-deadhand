4305551e619528d2908842f06c7849e6476c80bb
Vasiliy Shapovalov <vsh@MacBook-Pro-Vasiliy.local>
2020-05-29 16:31:43 +0300

    msg parser

---
+++ iljsonparser.py
@@ -0,0 +1,39 @@
+import ijson
+import pprint
+
+def traverse(obj, index, parent):
+    if isinstance(obj, dict):
+        return {k: traverse(v, k, obj) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [traverse(elem, index, obj) for index, elem in enumerate(obj)]
+    else:
+        parent[index] = ""
+        
+different_structures = []
+first_occurence = {}
+
+f = open('events.json')
+msgs = ijson.items(f, '', multiple_values = True)
+i = 0
+try: 
+    for msg in msgs:
+        msga = [msg]
+        traverse(msg, 0 , msga)
+        if msg not in different_structures:
+            different_structures.append(msg)
+            first_occurence[i] = msg
+    #    recv_count = 
+
+    #    senders = msg['message.sender']
+    #    print(senders)
+    #    recv_packet_count = len(msg.get('recv_packet.packet_data', []))
+    #    print(recv_packet_count)
+    #    print(msg)
+        i = i + 1
+except:
+    pass
+#cities = (o for o in objects if o['type'] == 'city')
+#for city in cities:
+#    do_something_with(city) 
+
+pprint.pprint(first_occurence)
