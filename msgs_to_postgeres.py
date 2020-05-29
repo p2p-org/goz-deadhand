f66fc0db46b87bb40b4fe19293b383042cf4bf00
Vasiliy Shapovalov <vsh@MacBook-Pro-Vasiliy.local>
2020-05-29 22:28:14 +0300

    aaa

--- msgs_to_postgeres.py
+++ msgs_to_postgeres.py
@@ -3,6 +3,7 @@ import pprint
 import argparse
 import json
 import psycopg2
+import os
 
 conn = psycopg2.connect(dbname='postgres', user='postgres', 
                         password='gozogozogoz', host='localhost')
@@ -18,6 +19,7 @@ args = parser.parse_args()
         
 different_structures = []
 first_occurence = {}
+filesize = os.stat(args.json_path).st_size
 
 f = open(args.json_path)
 msgs = ijson.items(f, '', multiple_values = True)
@@ -28,7 +30,8 @@ try:
         sql = "INSERT INTO json_test (data) VALUES (%s)"
         cursor.execute(sql, (my_json,))
         if i%1000 == 0:
-            print(i)
+            pos = f.tell()/filesize
+            print(i, "%.2f"%pos)
         #print (cursor.fetchone()[0])
         i = i+1
 except Exception as e:
