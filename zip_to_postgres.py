b20bc09d60ba1a51eceaf96ff367a5b90834efc7
Vasiliy Shapovalov <vsh@MacBook-Pro-Vasiliy.local>
2020-05-30 08:53:44 +0300

    zipload

--- zip_to_postgres.py
+++ zip_to_postgres.py
@@ -1,3 +1,4 @@
+import zipfile
 import ijson
 import pprint
 import argparse
@@ -21,15 +22,21 @@ different_structures = []
 first_occurence = {}
 filesize = os.stat(args.json_path).st_size
 
-f = open(args.json_path)
+zipf = zipfile.ZipFile(args.json_path)
+f=zipf.open("logs/events.log.phase2a_part2_fixed")
+
 msgs = ijson.items(f, '', multiple_values = True)
 i = 0
 try: 
     for msg in msgs:
         my_json = json.dumps(msg)
+        print(msg)
+        if(i == 5):
+            break
         sql = "INSERT INTO json_test (data) VALUES (%s)"
-        cursor.execute(sql, (my_json,))
+        #cursor.execute(sql, (my_json,))
         if i%1000 == 0:
+
             pos = 100*f.tell()/filesize
             print(i, "%.2f"%pos + "%")
         #print (cursor.fetchone()[0])
@@ -38,3 +45,6 @@ except Exception as e:
     print(e)
 conn.commit()
 print(i)
+
+
+
